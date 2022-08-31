744b93ce43e849734f54293913f82a64ab3fa2ea
Mukul Kolpe <mukulkolpe45@gmail.com>
2022-08-31 21:22:58 +0530

    Applied framer-motion for card animation

--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -2,37 +2,47 @@ import React from "react";
 import "./MovieList.css";
 import { ToastContainer, toast, Flip } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { motion, AnimatePresence } from "framer-motion";
 
 function MovieList(props) {
   const notify = () => toast.success("Movie Nominated!");
   const NomComponent = props.nomComponent;
   return (
     <>
-      <div className="movie-list">
-        {props.movies.map((movie, index) => (
-          <div className="movie">
-            <img
-              src={movie.Poster}
-              alt="Movie Poster"
-              className="movie-poster"
-            ></img>
-            <span className="movie-name">{movie.Title}</span>
-            <div className="movie-info">
-              <span className="info-span">Year: {movie.Year}</span>
-              <span className="info-span">{movie.Type}</span>
-            </div>
-            <button
-              onClick={() => {
-                props.handleNomClick(movie);
-                notify();
-              }}
-              className="nominateBtn"
+      <motion.div layout className="movie-list">
+        <AnimatePresence>
+          {props.movies.map((movie, index) => (
+            <motion.div
+              key={index}
+              layout
+              animate={{ opacity: 1, scale: 1 }}
+              initial={{ opacity: 0, scale: 0.9 }}
+              exit={{ opacity: 0, scale: 0.9 }}
+              className="movie"
             >
-              <NomComponent />
-            </button>
-          </div>
-        ))}
-      </div>
+              <img
+                src={movie.Poster}
+                alt="Movie Poster"
+                className="movie-poster"
+              ></img>
+              <span className="movie-name">{movie.Title}</span>
+              <div className="movie-info">
+                <span className="info-span">Year: {movie.Year}</span>
+                <span className="info-span">{movie.Type}</span>
+              </div>
+              <button
+                onClick={() => {
+                  props.handleNomClick(movie);
+                  notify();
+                }}
+                className="nominateBtn"
+              >
+                <NomComponent />
+              </button>
+            </motion.div>
+          ))}
+        </AnimatePresence>
+      </motion.div>
       <ToastContainer
         position="top-right"
         autoClose={500}
